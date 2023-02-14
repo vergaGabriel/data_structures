b282e1b11df258f6ca8c97e6c3c820fcb7c30f6b
vergaGabriel <gabriel_verga@hotmail.com>
2023-02-14 09:20:43 -0300

    Atualização do Arquivo

--- array/array_insert.py
+++ array/array_insert.py
@@ -13,11 +13,32 @@ class Insert:
                 We 'append' an element so that we can pass the others to the right, 
                 otherwise this 'append' will give an error that exceeded the number of elements.
             """
-            self.array.append(0)
+            self.array.append("a")
             self.array[index] = self.array[index - 1]
             index -= 1
+            
+            """
+                We eliminate the element that we 'append' because otherwise there would be a
+                difference in size and we would have this unwanted element at the end of the array.
+            """
+            if "a" in self.array:
+                self.array.pop()
+            else:
+                continue
 
         self.array[0] = element
+
+    def deleteNum(self):
+        """
+            This server function to remove an item from the first position in array.
+        """
+        index = len(self.array)
+        num = 0
+        while num < (index - 1):
+            self.array[num] = self.array[num + 1]
+            num += 1
+
+        self.array.pop()
 
     def item(self, value):
         return self.array[value]
